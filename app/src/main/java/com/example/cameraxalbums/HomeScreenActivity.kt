394497a4349ca51b0aca87677637fb642450d2f6
Abhinav Shukla <abhinav2014shukla@gmail.com>
2021-07-31 15:53:10 +0530

    Image Clicking code written. On to Showing the albums in Recycler View

--- app/src/main/java/com/example/cameraxalbums/HomeScreenActivity.kt
+++ app/src/main/java/com/example/cameraxalbums/HomeScreenActivity.kt
@@ -1,11 +1,49 @@
 package com.example.cameraxalbums
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.cameraxalbums.adapters.AlbumsListAdapter
+import com.example.cameraxalbums.database.Albums
+import com.example.cameraxalbums.listener.AlbumItemClickListener
+import com.example.cameraxalbums.viewModel.AlbumLogicViewModel
+import com.example.cameraxalbums.viewModel.ViewModelFactory
+import kotlinx.android.synthetic.main.activity_home_screen.*
 
-class HomeScreenActivity : AppCompatActivity() {
+class HomeScreenActivity : AppCompatActivity(), AlbumItemClickListener {
+    private lateinit var viewModel: AlbumLogicViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_screen)
+        initialisingTheViewModel()
+    }
+
+    private fun initialisingTheViewModel() {
+        viewModel = ViewModelFactory(this).create(AlbumLogicViewModel::class.java)
+        viewModel.fetchAllAlbums().observe(this, {
+            it?.let {
+                setRecyclerAdapter(it)
+            }
+        })
+    }
+
+    private fun setRecyclerAdapter(it: List<Albums>) {
+        val gridLayoutManager = GridLayoutManager(this, 3)
+        val albumsListAdapter = AlbumsListAdapter(it, this)
+        recyclerViewShowAlbums.apply {
+            layoutManager = gridLayoutManager
+            adapter = albumsListAdapter
+
+        }
+
+    }
+
+    override fun onItemClicked(albums: Albums) {
+        val albumName = albums.album_name
+        val intent = Intent(this, ShowImageActivity::class.java)
+        intent.putExtra("data", albumName)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/example/cameraxalbums/ShowImageActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cameraxalbums
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowImageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_image)
+    }
+}
